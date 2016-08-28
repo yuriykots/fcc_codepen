374663d9df4899c8033397e37fb8f654bb5e6061
yuriykots <setingm@gmail.com>
2016-08-28 14:41:53 -0400

    Simple board is done.

--- gameoflife.js
+++ gameoflife.js
@@ -1,15 +1,32 @@
+
+//Solution 1
+var cellsArray = [];
+for(var i=0; i<1500; i++){
+  cellsArray.push(0);
+}
+//Solution 2
+var cellsArray2 = new Array(1500).fill(0);
+
+//give each sell an id.
+//when sell is pressed, update state and rerender board with different color cell.
+
 var App = React.createClass({
 
+    getInitialState: function(){
+      return {
+        cells: cellsArray
+      }
+    },
 
-render: function() {
-  console.log(this.state.data)
-  return (
-    <div>
-    <h1> Hello </h1>
-    </div>
-  );
-}
+    render: function() {
+    return (
+
+      <div>
+       {this.state.cells.map(function(cell){
+          return <div className="cells"> </div>
+        })}
+      </div>
+    );
+  }
 });
-
-
 ReactDOM.render(<App/>, document.getElementById('app'));

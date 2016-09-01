c1b89da12c596928b36ecccf93635a284fcb250b
yuriykots <setingm@gmail.com>
2016-09-01 14:49:34 -0400

    put cells in separate react element

--- gameoflife.js
+++ gameoflife.js
@@ -4,11 +4,45 @@ var cellsArray = [];
 for(var i=0; i<1500; i++){
   cellsArray.push(0);
 }
+
+cellsArray[40]=1;
+cellsArray[151]=1;
+cellsArray[958]=1;
+cellsArray[773]=1;
+cellsArray[555]=1;
+cellsArray[648]=1;
 //Solution 2
 var cellsArray2 = new Array(1500).fill(0);
 
 //give each sell an id.
 //when sell is pressed, update state and rerender board with different color cell.
+//id={index} onChange={this.handleClick2(index)}
+
+var CellD = React.createClass({
+
+  handleClick: function() {
+      console.log("cell is pressed")
+  },
+
+  render: function() {
+    return (
+    <div className="cells-d" onClick={this.handleClick}> </div>
+    );
+    }
+  });
+
+var CellA = React.createClass({
+
+  handleClick: function() {
+      console.log("cell is pressed")
+  },
+
+  render: function() {
+    return (
+    <div className="cells-a" onClick={this.handleClick}></div>
+    );
+    }
+  });
 
 var App = React.createClass({
 
@@ -18,13 +52,28 @@ var App = React.createClass({
       }
     },
 
+   handleClick: function() {
+     var cellsArray = this.state.cells;
+     cellsArray[1] = 1;
+        this.setState({cells: cellsArray});
+  },
+
+    handleClick2: function(index) {
+      console.log("cell is pressed" + index)
+  },
+
     render: function() {
     return (
 
       <div>
-       {this.state.cells.map(function(cell){
-          return <div className="cells"> </div>
-        })}
+       {this.state.cells.map(function(cell, index) {
+          if (cell === 0){
+            return <CellD/>
+          }
+           if (cell === 1){
+            return <CellA/>
+          }
+        }.bind(this))}
       </div>
     );
   }
